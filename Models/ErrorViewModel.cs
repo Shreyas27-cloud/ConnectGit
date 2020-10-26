bd801ed2ef280438f9d1b76bf37b9a9a6c9185b1
DELL <DELL@DELL-PC>
2020-10-26 14:48:07 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConnectGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
